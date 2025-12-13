d1d94a2339aa5a47763ac96f51536a2bca81fe24
Eduardo Canchola <canchola700@gmail.com>
2025-12-13 13:19:45 -0600

    envoltura de MyApp en ProviderScope

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,13 @@
 
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import 'package:pokeapi_banpay_test/presentation/pages/list_page.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const ProviderScope(
+    child: MyApp(),
+  ));
 }
 
 class MyApp extends StatelessWidget {
@@ -13,7 +16,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      title: 'PokeApi',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
