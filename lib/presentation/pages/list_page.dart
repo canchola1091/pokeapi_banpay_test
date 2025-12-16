16a1f9766e6b39da23b8bb1d5919037e89517ac2
Eduardo Canchola <canchola700@gmail.com>
2025-12-15 22:54:01 -0600

    ajustes list_page, add favorite

--- lib/presentation/pages/list_page.dart
+++ lib/presentation/pages/list_page.dart
@@ -1,9 +1,18 @@
 
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
+
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:pokeapi_banpay_test/data/models/favorite_pokemon_model.dart';
+import 'package:pokeapi_banpay_test/data/models/pokemon_model.dart';
+
 import 'package:pokeapi_banpay_test/presentation/pages/detail_page.dart';
+import 'package:pokeapi_banpay_test/presentation/pages/favorites_page.dart';
+import 'package:pokeapi_banpay_test/presentation/providers/favorite_pokemon_provider.dart';
 import 'package:pokeapi_banpay_test/presentation/providers/pokemon_provider.dart';
 class ListPage extends ConsumerStatefulWidget {
+
   const ListPage({super.key});
 
   @override
@@ -26,33 +35,69 @@ class _ListPageState extends ConsumerState<ListPage> {
     final pokemonState = ref.watch(pokemonListNotifierProvider);
 
     return Scaffold(
-      appBar: AppBar(title: const Text('Pokédex')),
-      body: pokemonState.isLoading
-          ? const Center(child: CircularProgressIndicator())
-          : pokemonState.errorMessage != null
+      appBar: AppBar(
+        title: const Text('Lista de Pokemones'),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.favorite),
+            onPressed: () {
+              Navigator.of(context).push(
+                MaterialPageRoute(
+                  builder: (context) => const FavoritesPage(),
+                ),
+              );
+            },
+          ),
+        ],
+      ),
+      body: (pokemonState.isLoading)
+          ? const Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Center(child: CircularProgressIndicator()),
+              SizedBox(height: 15.0),
+              Text('Cargando, espere un momento...'),
+            ],
+          )
+          : (pokemonState.errorMessage != null)
               ? Center(child: Text('Error: ${pokemonState.errorMessage}'))
               : ListView.builder(
                   itemCount: pokemonState.pokemons?.length ?? 0,
                   itemBuilder: (context, index) {
-                    final pokemon = pokemonState.pokemons![index];
+
+                    final PokemonModel pokemon = pokemonState.pokemons![index];
+                    final String formatName = pokemon.name[0].toUpperCase() + pokemon.name.substring(1);
+
                     return ListTile(
                       leading: CircleAvatar(
                         backgroundImage: NetworkImage(pokemon.imageUrl),
                       ),
-                      title: Text(pokemon.name),
+                      trailing: IconButton(
+                        onPressed: () {
+                          log('Favorite button pressed for ${pokemon.name}');
+                          final favoritePokemon = FavoritePokemonModel.fromPokemonModel(pokemon);
+                          ref.read(favoritePokemonListNotifierProvider.notifier).addFavorite(favoritePokemon);
+                        },
+                        icon: const Icon(Icons.favorite_border)
+                      ),
+                      title: Text(formatName),
                       subtitle: Text('Types: ${pokemon.types.join(', ')}'),
                       onTap: () {
                         Navigator.of(context).push(
                           MaterialPageRoute(
                             builder: (context) => DetailPage(
                               pokemonId: pokemon.id,
-                              pokemonName: pokemon.name
+                              pokemonName: formatName
                             )
                           ),
                         );
                       },
                     );
                   },
+                ),
+                floatingActionButton: FloatingActionButton(
+                  child: const Icon(Icons.refresh),
+                  onPressed: () => ref.read(pokemonListNotifierProvider.notifier).loadPokemonList()
                 ),
     );
   }
