8d6d2776b9644565e491b6e4e854460968f663a3
Eduardo Canchola <canchola700@gmail.com>
2025-12-16 09:31:26 -0600

    se crea widget pokemon_tile

--- lib/presentation/pages/list_page.dart
+++ lib/presentation/pages/list_page.dart
@@ -2,14 +2,13 @@
 import 'package:flutter/material.dart';
 
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:pokeapi_banpay_test/data/models/favorite_pokemon_model.dart';
-import 'package:pokeapi_banpay_test/data/models/pokemon_model.dart';
 
-import 'package:pokeapi_banpay_test/presentation/pages/detail_page.dart';
+import 'package:pokeapi_banpay_test/data/models/pokemon_model.dart';
 import 'package:pokeapi_banpay_test/presentation/pages/favorites_page.dart';
 import 'package:pokeapi_banpay_test/presentation/providers/favorite_pokemon_provider.dart';
 import 'package:pokeapi_banpay_test/presentation/providers/pokemon_notifier.dart';
 import 'package:pokeapi_banpay_test/presentation/providers/pokemon_provider.dart';
+import 'package:pokeapi_banpay_test/presentation/widgets/list/pokemon_tile.dart';
 import 'package:pokeapi_banpay_test/presentation/widgets/shared/loading_widget.dart';
 class ListPage extends ConsumerStatefulWidget {
 
@@ -55,51 +54,26 @@ class _ListPageState extends ConsumerState<ListPage> {
         ] : [],
       ),
       body: (pokemonState.isLoading)
-          ? const LoadingWidget()
-          : (pokemonState.errorMessage != null)
-              ? Center(child: Text('Error: ${pokemonState.errorMessage}'))
-              : ListView.builder(
-                  itemCount: pokemonState.pokemons?.length ?? 0,
-                  itemBuilder: (context, index) {
+      ? const LoadingWidget()
+      : (pokemonState.errorMessage != null)
+        ? Center(child: Text('Error: ${pokemonState.errorMessage}'))
+        : ListView.builder(
+            itemCount: pokemonState.pokemons?.length ?? 0,
+            itemBuilder: (context, index) {
 
-                    final PokemonModel pokemon = pokemonState.pokemons![index];
-                    final isFavorite = favoriteIds.contains(pokemon.id);
-                    final String formatName = pokemon.name[0].toUpperCase() + pokemon.name.substring(1);
+              final PokemonModel pokemon = pokemonState.pokemons![index];
+              final isFavorite = favoriteIds.contains(pokemon.id);
 
-                    return ListTile(
-                      leading: CircleAvatar(
-                        backgroundImage: NetworkImage(pokemon.imageUrl),
-                      ),
-                      trailing: IconButton(
-                        onPressed: () {
-                          final favoritePokemon = FavoritePokemonModel.fromPokemonModel(pokemon);
-                          if (isFavorite) {
-                            ref.read(favoritePokemonListNotifierProvider.notifier).removeFavorite(pokemon.id);
-                          } else {
-                            ref.read(favoritePokemonListNotifierProvider.notifier).addFavorite(favoritePokemon);
-                          }
-                        },
-                        icon: Icon(Icons.favorite, color: (isFavorite) ? Colors.red : Colors.grey)
-                      ),
-                      title: Text(formatName),
-                      subtitle: Text('Types: ${pokemon.types.join(', ')}'),
-                      onTap: () {
-                        Navigator.of(context).push(
-                          MaterialPageRoute(
-                            builder: (context) => DetailPage(
-                              pokemonId: pokemon.id,
-                              pokemonName: formatName
-                            )
-                          ),
-                        );
-                      },
-                    );
-                  },
-                ),
-                floatingActionButton: FloatingActionButton(
-                  child: const Icon(Icons.refresh),
-                  onPressed: () => ref.read(pokemonListNotifierProvider.notifier).loadPokemonList()
-                ),
+              return PokemonTile(
+                pokemonModel: pokemon,
+                isFavoritePokemon: isFavorite
+              );
+            }
+          ),
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.refresh),
+        onPressed: () => ref.read(pokemonListNotifierProvider.notifier).loadPokemonList()
+      )
     );
   }
 }
---
+++ lib/presentation/widgets/list/pokemon_tile.dart
@@ -0,0 +1,62 @@
+
+import 'package:flutter/material.dart';
+
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import 'package:pokeapi_banpay_test/data/models/favorite_pokemon_model.dart';
+import 'package:pokeapi_banpay_test/data/models/pokemon_model.dart';
+import 'package:pokeapi_banpay_test/presentation/pages/detail_page.dart';
+import 'package:pokeapi_banpay_test/presentation/providers/favorite_pokemon_provider.dart';
+
+class PokemonTile extends ConsumerStatefulWidget {
+
+  final PokemonModel pokemonModel;
+  final bool isFavoritePokemon;
+  
+  const PokemonTile({
+    required this.pokemonModel,
+    required this.isFavoritePokemon,
+    super.key
+  });
+
+  @override
+  ConsumerState<ConsumerStatefulWidget>  createState() => _PokemonItemState();
+}
+
+class _PokemonItemState extends ConsumerState<PokemonTile> {
+
+  @override
+  Widget build(BuildContext context) {
+
+    final String formatName = widget.pokemonModel.name[0].toUpperCase() + widget.pokemonModel.name.substring(1);
+
+    return ListTile(
+      leading: CircleAvatar(
+        backgroundImage: NetworkImage(widget.pokemonModel.imageUrl),
+      ),
+      trailing: IconButton(
+        onPressed: () {
+          final favoritePokemon = FavoritePokemonModel.fromPokemonModel(widget.pokemonModel);
+          if (widget.isFavoritePokemon) {
+            ref.read(favoritePokemonListNotifierProvider.notifier).removeFavorite(widget.pokemonModel.id);
+          } else {
+            ref.read(favoritePokemonListNotifierProvider.notifier).addFavorite(favoritePokemon);
+          }
+        },
+        icon: Icon(Icons.favorite, color: (widget.isFavoritePokemon) ? Colors.red : Colors.grey)
+      ),
+      title: Text(formatName),
+      subtitle: Text('Types: ${widget.pokemonModel.types.join(', ')}'),
+      onTap: () {
+        Navigator.of(context).push(
+          MaterialPageRoute(
+            builder: (context) => DetailPage(
+              pokemonId: widget.pokemonModel.id,
+              pokemonName: formatName
+            )
+          )
+        );
+      }
+    );
+  }
+}
